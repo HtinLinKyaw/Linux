2ff40270359c06ee1af1ab451d6a529dbae52008
HtinLinKyaw <hlk3958@gmail.com>
2020-11-10 11:33:47 +0630

    Update Lubuntu16.sh

--- Lubuntu16.sh
+++ Lubuntu16.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 echo This Scripts is Written By Linux VPS Myanmar
-sudo apt update
-sudo apt upgrade -y
-sudo apt install tasksel xrdp -y
-sudo tasksel install lubuntu-desktop
-sudo dpkg --add-architecture i386
-sudo wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial main'
-sudo apt update
-sudo apt install --install-recommends winehq-stable -y
-reboot
